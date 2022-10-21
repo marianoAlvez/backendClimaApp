6e509ab7ee5a773af59495dc654e73e641808076
marianoAlvez <alvez_jmariano@hotmail.com>
2022-10-21 16:42:31 -0300

    add menu

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-const {inquirerMenu, pause } = require("./helpers/inquirer");
+const Searches = require("./models/searches");
+const {inquirerMenu, readInput } = require("./helpers/inquirer");
 
 
-const main = async()=> {
+const main = async () => {
+    const searches = new Searches();
     let option;
 
     do{
         option = await inquirerMenu();
-        await pause();
+        switch(option) {
+            case 1:
+                const place = await readInput('City: ');
+                console.log(place);
+                console.log('\nCity information\n'.green);
+                console.log('City');
+                console.log('Latitude');
+                console.log('Longitude');
+                console.log('Temperature');
+                console.log('Maximum');
+                console.log('Minimum');
+            case 2:
+                
+            break;
+
+        }
     
     } while( option !== 0 );
 
---
+++ models/searches.js
@@ -0,0 +1,15 @@
+class Searches {
+    records = ['Obera' , 'Alem'];
+
+    constructor(){
+    //todo: Leer DB si existe
+    }
+
+    async city( place = ''){
+        //peticion http
+        console.log(pleace);
+        return []; //retornar lugares que coincidan con la busqueda
+    }
+}
+
+module.exports = Searches;
